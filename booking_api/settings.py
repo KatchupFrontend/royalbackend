98947446812a923b2924afbb1afd52f35361fd35
EbenezerOffei <ebenezeroffei@outlook.com>
2022-12-16 10:47:10 +0000

    Modified settings file

--- booking_api/settings.py
+++ booking_api/settings.py
@@ -142,4 +142,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
-CSRF_TRUSTED_ORIGINS = ["https://royalbackend-production.up.railway.app"];
+CSRF_TRUSTED_ORIGINS = ["*.railway.app"];
